a7dcd815a846746f552a9f9642de74c33713be84
EVEN <evensalew07@yahoo.com>
2022-02-01 21:41:08 -0500

    second integration

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -2,14 +2,15 @@ package virtual_pet;
 
 public class VirtualPet {
 
-        private String breed;
-        private String furColor;
-        private int age;
-        private int hunger;
-        private String name;
-        private int thirst;
-        private int boredom;
 
+    private String breed;
+    private String furColor;
+    private int age;
+    private int hunger;
+    private String name;
+    private int thirst;
+    private int boredom;
+    private boolean dead;
 
     public VirtualPet(String breed, String furColor, int age, int hunger, String name, int thirst, int boredom) {
         this.breed = breed;
@@ -19,6 +20,7 @@ public class VirtualPet {
         this.name = name;
         this.thirst = thirst;
         this.boredom = boredom;
+        this.dead = false;
     }
 
     public int getThirst() {
@@ -30,39 +32,53 @@ public class VirtualPet {
     }
 
     public String getBreed() {
-            return breed;
-        }
+        return breed;
+    }
 
-        public String getFurColor() {
-            return furColor;
-        }
+    public String getFurColor() {
+        return furColor;
+    }
 
-        public int getAge() {
-            return age;
-        }
+    public int getAge() {
+        return age;
+    }
 
-        public int getHunger() {
-            return hunger;
-        }
+    public int getHunger() {
+        return hunger;
+    }
 
-        public String getName() {
-            return name;
-        }
-        public void tick(){
-             hunger++;
-             thirst++;
-             boredom++;
-        }
-        public void feed(){
-        hunger -= 5;
-        }
-        public void play(){
-        boredom -= 10;
-        }
-        public void water(){
-        thirst -= 15;
+    public String getName() {
+        return name;
+    }
+
+    public void tick() {
+        hunger++;
+        thirst++;
+        boredom++;
+        if(hunger > 20 || thirst>20 ||boredom > 20){
+            dead = true;
         }
     }
+
+    public void feed() {
+        hunger -= 5;
+    }
+
+    public void play() {
+        boredom -= 10;
+    }
+
+    public void water() {
+        thirst -= 15;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+    public void isAdopted(){
+        return;
+    }
+}
 
 
 
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -15,30 +15,50 @@ public class VirtualPetApplication {
 
     }
   public void play(){
+        PetShelter myShelter = new PetShelter();
       System.out.println("Welcome to the game");
-       VirtualPet dog = new VirtualPet("German","Brown",5,8,"Vicki",10,10);
-
-    while(dog.getHunger()<20){
-        System.out.println("Hunger "+ dog.getHunger()+ " thirst " + dog.getThirst()+ " boredom " + dog.getBoredom());
+       myShelter.addPetToShelter(new VirtualPet("German","Brown",5,8,"Vicki",10,10));
+       myShelter.addPetToShelter(new VirtualPet("british","Black",6,10,"Bobby",10,5));
+       myShelter.addPetToShelter(new VirtualPet("irish","grey",10,11,"ruthie",11,5));
+       myShelter.addPetToShelter(new VirtualPet("Italian","white",5,15,"Jackie",12,10));
+    while(myShelter.allPetsNotDead()){
+        displayStats(myShelter);
         System.out.println("What do you want to do with the pet?");
         System.out.println("1 feed");
         System.out.println("2 play");
         System.out.println("3 water");
+        System.out.println("4 add pet");
+        System.out.println("5 adopt pet");
         System.out.println("0 exit");
         int choice = inputScanner.nextInt();
+        inputScanner.nextLine();
         if(choice == 1) {
-            dog.feed();
+            myShelter.feedAllPets();
         }
         else if (choice == 2){
-             dog.play();
+             myShelter.playAllPets();
          }
         else if (choice == 3){
-            dog.water();
+            myShelter.waterAllPets();
+        }
+        else if (choice == 4) {
+            myShelter.addAllPets();
+        }
+        else if (choice == 5){
+            System.out.println("Which Pet do you want to adopt?");
+            myShelter.removePet(inputScanner.nextLine());
         }
         else {
             break;
         }
-        dog.tick();
+        myShelter.tickAllPets();
     }
+  }
+  public void displayStats(PetShelter shelter){
+      System.out.println("****Stats*******************************************************");
+        for(VirtualPet currentPet: shelter.getShelter()){
+            System.out.println(currentPet.getName()+": Hunger "+ currentPet.getHunger()+ " thirst " + currentPet.getThirst()+ " boredom " + currentPet.getBoredom());
+        }
+      System.out.println("*******************************************************");
   }
 }
